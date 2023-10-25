f20a8499ba7490cbd9d2cc131410eeea749a2c4e
infimum05772 <r.arifulina@gmail.com>
2023-10-25 11:59:22 +0300

    phone mask match checking added

--- app/src/main/java/com/itis/android_tasks/ui/fragments/StartPageFragment.kt
+++ app/src/main/java/com/itis/android_tasks/ui/fragments/StartPageFragment.kt
@@ -36,7 +36,7 @@ class StartPageFragment : Fragment(R.layout.fragment_start_page) {
     }
 
     private fun validatePhone(phone: String): Boolean {
-        return phone.length == 18
+        return phone.length == 18 && phone.matches(Regex("^\\+7 \\(9\\d{2}\\)-\\d{3}-\\d{2}-\\d{2}$"))
     }
 
     private fun validateQuestionsAmount(questionsAmount: String): Boolean {
@@ -152,7 +152,7 @@ class StartPageFragment : Fragment(R.layout.fragment_start_page) {
             etPhone.setOnFocusChangeListener { _, isFocused ->
                 if (!isFocused) {
                     if (!validatePhone(etPhone.text.toString())) {
-                        tilPhone.error = "phone number must contain 10 digits"
+                        tilPhone.error = "phone number must contain 10 digits and match mask\n+7 (9##)-###-##-##"
                     }
                 } else if (etPhone.text.isEmpty()) {
                     etPhone.setText("+7 (9")
