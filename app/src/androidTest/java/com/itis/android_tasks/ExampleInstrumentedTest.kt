abc0563e681538a7e2a2e25dbe9bc7b026bb7007
Регина Арифулина <113761346+infimum05772@users.noreply.github.com>
2023-11-23 23:39:53 +0300

    Revert "Revert "Homework 4""

--- app/src/androidTest/java/com/itis/android_tasks/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/itis/android_tasks/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.itis.myapplication
+package com.itis.android_tasks
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/com/itis/android_tasks/ui/fragments/CoroutinesSettingsPageFragment.kt
@@ -0,0 +1,122 @@
+package com.itis.android_tasks.ui.fragments
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.itis.android_tasks.MainActivity
+import com.itis.android_tasks.R
+import com.itis.android_tasks.databinding.FragmentCoroutinesSettingsPageBinding
+import com.itis.android_tasks.model.settings.CoroutinesSettings
+import com.itis.android_tasks.utils.AirplaneModeChangingListener
+
+class CoroutinesSettingsPageFragment : Fragment(R.layout.fragment_coroutines_settings_page) {
+
+    private var _binding: FragmentCoroutinesSettingsPageBinding? = null
+    private val binding: FragmentCoroutinesSettingsPageBinding
+        get() = _binding!!
+
+    private val coroutinesSettings: CoroutinesSettings = CoroutinesSettings
+
+    private var airplaneModeChangingListener: AirplaneModeChangingListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCoroutinesSettingsPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        airplaneModeChangingListener = AirplaneModeChangingListener(
+            requireContext(),
+            onAirplaneModeChanged = {
+                binding.btnStartCoroutines.isEnabled = !it
+            }
+        ).also {
+            it.onStartAirplaneModeCheck()
+        }
+
+        initCoroutinesSettings()
+        initCoroutinesCountChangedListener()
+        initIsAsyncStateChangedListener()
+        initIsStoppedOnBackgroundStateChangedListener()
+
+        binding.btnStartCoroutines.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED) {
+                (activity as? MainActivity)?.requestPermission(Manifest.permission.POST_NOTIFICATIONS)
+            }
+            (activity as? MainActivity)?.startCoroutines()
+        }
+    }
+
+    private fun initCoroutinesSettings() {
+        with(binding) {
+            sbCoroutinesCount.progress = coroutinesSettings.count
+            tvCoroutineCountProgress.text = coroutinesSettings.count.toString()
+            chbAsync.isChecked = coroutinesSettings.isAsync
+            chbStopOnBg.isChecked = coroutinesSettings.isStoppedOnBackground
+        }
+    }
+
+    private fun initCoroutinesCountChangedListener() {
+        with(binding) {
+            sbCoroutinesCount.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    var trueProgress = progress
+                    if (progress < 1){
+                        seekBar?.progress = 1
+                        trueProgress = 1
+                    }
+                    tvCoroutineCountProgress.text = trueProgress.toString()
+                    coroutinesSettings.count = trueProgress
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            })
+        }
+    }
+
+    private fun initIsAsyncStateChangedListener() {
+        binding.chbAsync.setOnCheckedChangeListener { _, isChecked ->
+            coroutinesSettings.isAsync = isChecked
+        }
+    }
+
+    private fun initIsStoppedOnBackgroundStateChangedListener() {
+        binding.chbStopOnBg.setOnCheckedChangeListener { _, isChecked ->
+            coroutinesSettings.isStoppedOnBackground = isChecked
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        requireContext().unregisterReceiver(airplaneModeChangingListener?.receiver)
+        super.onDestroyView()
+    }
+
+    companion object {
+        const val COROUTINES_SETTINGS_PAGE_FRAGMENT_TAG = "COROUTINES_SETTINGS_PAGE_FRAGMENT_TAG"
+    }
+}
